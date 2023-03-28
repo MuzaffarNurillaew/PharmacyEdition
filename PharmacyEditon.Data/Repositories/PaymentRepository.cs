2bc30c3ea12f616f88beb1265e11cfda58915850
wonderboy1w3 <wonderboy1w3@gmail.com>
2023-03-28 11:05:50 +0500

    PaymentRepository added

--- PharmacyEditon.Data/Repositories/PaymentRepository.cs
+++ PharmacyEditon.Data/Repositories/PaymentRepository.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using PharmacyEdition.Domain.Entities;
+using PharmacyEditon.Data.AppDbContext;
+using PharmacyEditon.Data.IRepositories;
 
 namespace PharmacyEditon.Data.Repositories
 {
-    internal class PaymentRepository
+    public class PaymentRepository : IPaymentRepository
     {
+        private readonly PharmacyDbContext context = new PharmacyDbContext();
+        public async Task<bool> DeleteAsync(Predicate<Payment> predicate)
+        {
+            if (predicate == null)
+            {
+                predicate = x => true;
+            }
+
+            var entityToDelete = this.context.Payments.ToList().Where(x => predicate(x)).ToList();
+
+            if (entityToDelete is null)
+            {
+                return false;
+            }
+
+            context.Payments.RemoveRange(entityToDelete);
+            await context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Payment> InsertAsync(Payment entity)
+        {
+            var insertedEntity = await context.Payments.AddAsync(entity);
+            await context.SaveChangesAsync();
+
+            return insertedEntity.Entity;
+        }
+
+        public List<Payment> SelectAllAsync(Predicate<Payment> predicate = null)
+        {
+            if (predicate == null)
+            {
+                predicate = x => true;
+            }
+            return this.context.Payments.Where(x => predicate(x)).ToList();
+        }
+
+        public async Task<Payment> SelectAsync(Predicate<Payment> predicate = null)
+        {
+            if (predicate is null)
+            {
+                predicate = x => true;
+            }
+            return await context.Payments.FirstOrDefaultAsync(x => predicate(x));
+        }
+
+        public async Task<Payment> UpdateAsync(long id, Payment entity)
+        {
+            var updatedEntity = context.Payments.Update(entity);
+            await context.SaveChangesAsync();
+
+            return updatedEntity.Entity;
+        }
     }
 }
