1c7e51073bdf322c1a49a2b3699661f369ba65a3
wonderboy1w3 <wonderboy1w3@gmail.com>
2023-03-28 11:33:07 +0500

    Changes tested

--- PharmacyEdition.Service/Services/OrderService.cs
+++ PharmacyEdition.Service/Services/OrderService.cs
@@ -1,170 +1,170 @@
-﻿using PharmacyEdition.Data.IRepositories;
-using PharmacyEdition.Data.Repositories;
-using PharmacyEdition.Domain.Entities;
-using PharmacyEdition.Domain.Enums;
-using PharmacyEdition.Service.DTOs;
-using PharmacyEdition.Service.Helpers;
-using PharmacyEdition.Service.Interfaces;
+﻿//using PharmacyEdition.Data.IRepositories;
+//using PharmacyEdition.Data.Repositories;
+//using PharmacyEdition.Domain.Entities;
+//using PharmacyEdition.Domain.Enums;
+//using PharmacyEdition.Service.DTOs;
+//using PharmacyEdition.Service.Helpers;
+//using PharmacyEdition.Service.Interfaces;
 
-namespace PharmacyEdition.Service.Services;
+//namespace PharmacyEdition.Service.Services;
 
-public class OrderService : IOrderService
-{
-    private readonly GenericRepository<Order> genericRepository = new GenericRepository<Order>();
-    private readonly IPaymentService paymentService = new PaymentService();
+//public class OrderService : IOrderService
+//{
+//    private readonly GenericRepository<Order> genericRepository = new GenericRepository<Order>();
+//    private readonly IPaymentService paymentService = new PaymentService();
 
-    public async Task<Response<Order>> CreateAsync(OrderCreationDto order)
-    {
-        // Checking that there are enough medicines or they exist
-        foreach (var med in order.Medicines)
-        {
-            var ourMed = (await medicineService.GetByIdAsync(med.Id)).Value;
+//    public async Task<Response<Order>> CreateAsync(OrderCreationDto order)
+//    {
+//        // Checking that there are enough medicines or they exist
+//        foreach (var med in order.Medicines)
+//        {
+//            var ourMed = (await medicineService.GetByIdAsync(med.Id)).Value;
 
-            // Checking for existence of medicine
-            if (ourMed is null)
-                return new Response<Order>
-                {
-                    StatusCode = 409,
-                    Message = "Some of these medicines does not exist",
-                    Value = null
-                };
+//            // Checking for existence of medicine
+//            if (ourMed is null)
+//                return new Response<Order>
+//                {
+//                    StatusCode = 409,
+//                    Message = "Some of these medicines does not exist",
+//                    Value = null
+//                };
 
-            // Checking does the amount of medicine enought
-            if (ourMed.Count < med.Count)
-                return new Response<Order>
-                {
-                    StatusCode = 410,
-                    Message = "Some of these medicine does not enough",
-                    Value = null
-                };
-        }
+//            // Checking does the amount of medicine enought
+//            if (ourMed.Count < med.Count)
+//                return new Response<Order>
+//                {
+//                    StatusCode = 410,
+//                    Message = "Some of these medicine does not enough",
+//                    Value = null
+//                };
+//        }
 
-        // Decreasing count of all medicines
-        foreach (var med in order.Medicines)
-        {
-            var ourMed = (await medicineService.GetByIdAsync(med.Id)).Value;
+//        // Decreasing count of all medicines
+//        foreach (var med in order.Medicines)
+//        {
+//            var ourMed = (await medicineService.GetByIdAsync(med.Id)).Value;
 
-            var mappedMed = new MedicineCreationDto
-            {
-                // Decreasing the count
-                Count = ourMed.Count - med.Count,
-                Description = ourMed.Description,
-                Name = ourMed.Name,
-                Price = ourMed.Price
-            };
-            await medicineService.UpdateAsync(ourMed.Id, mappedMed);
-        }
+//            var mappedMed = new MedicineCreationDto
+//            {
+//                // Decreasing the count
+//                Count = ourMed.Count - med.Count,
+//                Description = ourMed.Description,
+//                Name = ourMed.Name,
+//                Price = ourMed.Price
+//            };
+//            await medicineService.UpdateAsync(ourMed.Id, mappedMed);
+//        }
     
-        var paymentResult = await paymentService.CreateAsync(order.Payment);
+//        var paymentResult = await paymentService.CreateAsync(order.Payment);
 
-        if (paymentResult.Value.IsPaid)
-        {
-            var mappedOrder = new Order()
-            {
-                Medicines = order.Medicines,
-                Payment = paymentResult.Value,
-                Status = StatusType.Pending
-            };
-            var orderResult = await genericRepository.CreateAsync(mappedOrder);
+//        if (paymentResult.Value.IsPaid)
+//        {
+//            var mappedOrder = new Order()
+//            {
+//                Medicines = order.Medicines,
+//                Payment = paymentResult.Value,
+//                Status = StatusType.Pending
+//            };
+//            var orderResult = await genericRepository.CreateAsync(mappedOrder);
 
-            return new Response<Order>()
-            {
-                StatusCode = 200,
-                Message = "Success",
-                Value = orderResult
-            };
-        }
+//            return new Response<Order>()
+//            {
+//                StatusCode = 200,
+//                Message = "Success",
+//                Value = orderResult
+//            };
+//        }
 
-        return new Response<Order>()
-        {
-            StatusCode = 402,
-            Message = "Payment is not valid",
-            Value = null
-        };
-    }
+//        return new Response<Order>()
+//        {
+//            StatusCode = 402,
+//            Message = "Payment is not valid",
+//            Value = null
+//        };
+//    }
 
-    public async Task<Response<List<Order>>> GetAllAsync()
-    {
-        var orders = await this.genericRepository.GetAllAsync();
-        return new Response<List<Order>>()
-        {
-            StatusCode = 200,
-            Message = "Success",
-            Value = orders
-        };
-    }
+//    public async Task<Response<List<Order>>> GetAllAsync()
+//    {
+//        var orders = await this.genericRepository.GetAllAsync();
+//        return new Response<List<Order>>()
+//        {
+//            StatusCode = 200,
+//            Message = "Success",
+//            Value = orders
+//        };
+//    }
 
-    public async Task<Response<Order>> GetByIdAsync(long id)
-    {
-        var order = await this.genericRepository.GetByIdAsync(id);
-        if(order is null)
-            return new Response<Order>()
-            {
-                StatusCode = 404,
-                Message = "Success",
-                Value = null
-            };
+//    public async Task<Response<Order>> GetByIdAsync(long id)
+//    {
+//        var order = await this.genericRepository.GetByIdAsync(id);
+//        if(order is null)
+//            return new Response<Order>()
+//            {
+//                StatusCode = 404,
+//                Message = "Success",
+//                Value = null
+//            };
 
-        return new Response<Order>()
-        {
-            StatusCode = 200,
-            Message = "Success",
-            Value = order
-        };
-    }
+//        return new Response<Order>()
+//        {
+//            StatusCode = 200,
+//            Message = "Success",
+//            Value = order
+//        };
+//    }
 
-    public async Task<Response<Order>> UpdateAsync(long id, OrderCreationDto order)
-    {
-        var model = await this.genericRepository.GetByIdAsync(id);
-        if (model is null)
-            return new Response<Order>()
-            {
-                StatusCode = 404,
-                Message = "Success",
-                Value = null
-            };
-        var mappedPayment = new Payment()
-        {
-            OrderId = order.Payment.OrderId,
-            Type = order.Payment.Type,
-            IsPaid = order.Payment.IsPaid
-        };
-        var mappedOrder = new Order()
-        {
-            Medicines = order.Medicines,
-            Payment = mappedPayment,
-            Status = model.Status,
-        };
+//    public async Task<Response<Order>> UpdateAsync(long id, OrderCreationDto order)
+//    {
+//        var model = await this.genericRepository.GetByIdAsync(id);
+//        if (model is null)
+//            return new Response<Order>()
+//            {
+//                StatusCode = 404,
+//                Message = "Success",
+//                Value = null
+//            };
+//        var mappedPayment = new Payment()
+//        {
+//            OrderId = order.Payment.OrderId,
+//            Type = order.Payment.Type,
+//            IsPaid = order.Payment.IsPaid
+//        };
+//        var mappedOrder = new Order()
+//        {
+//            Medicines = order.Medicines,
+//            Payment = mappedPayment,
+//            Status = model.Status,
+//        };
 
-        var orderResult = await this.genericRepository.UpdateAsync(id, mappedOrder);
-        return new Response<Order>()
-        {
-            StatusCode = 200,
-            Message = "Success",
-            Value = orderResult
-        };
-    }
+//        var orderResult = await this.genericRepository.UpdateAsync(id, mappedOrder);
+//        return new Response<Order>()
+//        {
+//            StatusCode = 200,
+//            Message = "Success",
+//            Value = orderResult
+//        };
+//    }
 
-    public async Task<Response<Order>> ChangeStatus(long id, StatusType status)
-    {
-        var order = await genericRepository.GetByIdAsync(id);
-        if(order is not null)
-        {
-            order.Status = status;
-            var orderResult = await genericRepository.UpdateAsync(id, order);
-            return new Response<Order>()
-            {
-                StatusCode = 200,
-                Message = "Success",
-                Value = orderResult
-            };
-        }
+//    public async Task<Response<Order>> ChangeStatus(long id, StatusType status)
+//    {
+//        var order = await genericRepository.GetByIdAsync(id);
+//        if(order is not null)
+//        {
+//            order.Status = status;
+//            var orderResult = await genericRepository.UpdateAsync(id, order);
+//            return new Response<Order>()
+//            {
+//                StatusCode = 200,
+//                Message = "Success",
+//                Value = orderResult
+//            };
+//        }
 
-        return new Response<Order>()
-        {
-            StatusCode = 404,
-            Message = "Order is not found",
-            Value = null
-        };
-    }
-}
+//        return new Response<Order>()
+//        {
+//            StatusCode = 404,
+//            Message = "Order is not found",
+//            Value = null
+//        };
+//    }
+//}
--- PharmacyEdition/Program.cs
+++ PharmacyEdition/Program.cs
@@ -1,41 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//// See https://aka.ms/new-console-template for more information
 
-using PharmacyEdition.Data.IRepositories;
-using PharmacyEdition.Data.Repositories;
-using PharmacyEdition.Domain.Enums;
-using PharmacyEdition.Models;
-using PharmacyEdition.Service.DTOs;
-using PharmacyEdition.Service.Interfaces;
-using PharmacyEdition.Service.Services;
+//using PharmacyEdition.Data.IRepositories;
+//using PharmacyEdition.Data.Repositories;
+//using PharmacyEdition.Domain.Enums;
+//using PharmacyEdition.Models;
+//using PharmacyEdition.Service.DTOs;
+//using PharmacyEdition.Service.Interfaces;
+//using PharmacyEdition.Service.Services;
 
-GenericRepository<Medicine> medicineRepository = new GenericRepository<Medicine>();
+//GenericRepository<Medicine> medicineRepository = new GenericRepository<Medicine>();
 
-IMedicineService medicineService = new MedicineService();
-IOrderService orderService = new OrderService();
+//IMedicineService medicineService = new MedicineService();
+//IOrderService orderService = new OrderService();
 
-MedicineCreationDto medicineDto = new MedicineCreationDto()
+//MedicineCreationDto medicineDto = new MedicineCreationDto()
+//{
+//    Name = "Parestamol",
+//    Count = 10,
+//    Description = "Bosh",
+//    Price = 1000
+//};
+
+//var paymentDto = new PaymentCreationDto()
+//{
+//    IsPaid = true,
+//    OrderId = 2,
+//    Type = PaymentType.Humo
+//};
+
+//var medicine = await medicineService.GetByIdAsync(1);
+//var medicine2 = await medicineService.GetByIdAsync(2);
+
+//OrderCreationDto orderCreationDto = new OrderCreationDto()
+//{
+//    Medicines = new List<Medicine>() { medicine.Value, medicine2.Value },
+//    Payment = paymentDto
+//};
+
+////await medicineService.CreateAsync(medicineDto);
+//await orderService.CreateAsync(orderCreationDto);
+
+
+using PharmacyEdition.Domain.Entities;
+using PharmacyEditon.Data.IRepositories;
+using PharmacyEditon.Data.Repositories;
+
+IUserRepository ur = new UserRepository();
+
+await ur.InsertAsync(new User()
 {
-    Name = "Parestamol",
-    Count = 10,
-    Description = "Bosh",
-    Price = 1000
-};
-
-var paymentDto = new PaymentCreationDto()
-{
-    IsPaid = true,
-    OrderId = 2,
-    Type = PaymentType.Humo
-};
-
-var medicine = await medicineService.GetByIdAsync(1);
-var medicine2 = await medicineService.GetByIdAsync(2);
-
-OrderCreationDto orderCreationDto = new OrderCreationDto()
-{
-    Medicines = new List<Medicine>() { medicine.Value, medicine2.Value },
-    Payment = paymentDto
-};
-
-//await medicineService.CreateAsync(medicineDto);
-await orderService.CreateAsync(orderCreationDto);
+    Age = 18,
+    FirstName = "Muzaffar",
+    LastName = "Nurillayev",
+    Phone = "234",
+    Password = "password"
+});
