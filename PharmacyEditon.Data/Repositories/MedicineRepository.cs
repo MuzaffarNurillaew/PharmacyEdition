dbfb7cf6f10928f2f5080760f42426cb8a2fb06b
wonderboy1w3 <wonderboy1w3@gmail.com>
2023-03-28 10:59:15 +0500

    MedicineRepository completed!

--- PharmacyEditon.Data/Repositories/MedicineRepository.cs
+++ PharmacyEditon.Data/Repositories/MedicineRepository.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using PharmacyEdition.Models;
+using PharmacyEditon.Data.AppDbContext;
+using PharmacyEditon.Data.IRepositories;
 
 namespace PharmacyEditon.Data.Repositories
 {
-    internal class MedicineRepository
+    public class MedicineRepository : IMedicineRepository
     {
+        private readonly PharmacyDbContext context = new PharmacyDbContext();
+        public async Task<bool> DeleteAsync(Predicate<Medicine> predicate)
+        {
+            if (predicate == null)
+            {
+                predicate = x => true;
+            }
+
+            var entityToDelete = this.context.Medicines.ToList().Where(x => predicate(x)).ToList();
+
+            if (entityToDelete is null)
+            {
+                return false;
+            }
+
+            context.Medicines.RemoveRange(entityToDelete);
+            await context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Medicine> InsertAsync(Medicine entity)
+        {
+            var insertedEntity = await context.Medicines.AddAsync(entity);
+            await context.SaveChangesAsync();
+
+            return insertedEntity.Entity;
+        }
+
+        public List<Medicine> SelectAllAsync(Predicate<Medicine> predicate = null)
+        {
+            if (predicate == null)
+            {
+                predicate = x => true;
+            }
+            return this.context.Medicines.Where(x => predicate(x)).ToList();
+        }
+
+        public async Task<Medicine> SelectAsync(Predicate<Medicine> predicate = null)
+        {
+            if (predicate is null)
+            {
+                predicate = x => true;
+            }
+            return await context.Medicines.FirstOrDefaultAsync(x => predicate(x));
+        }
+
+        public async Task<Medicine> UpdateAsync(long id, Medicine entity)
+        {
+            var updatedEntity = context.Medicines.Update(entity);
+            await context.SaveChangesAsync();
+
+            return updatedEntity.Entity;
+        }
     }
 }
