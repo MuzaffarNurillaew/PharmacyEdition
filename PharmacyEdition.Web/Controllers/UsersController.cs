e8b787fbcd519cf5be0711bfb368c58f92b6d8d3
wonderboy1w3 <jamshidkhujqulov@gmail.com>
2023-03-31 01:55:37 +0500

    Add Web Layer

---
+++ PharmacyEdition.Web/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PharmacyEdition.Service.DTOs;
+using PharmacyEdition.Service.Interfaces;
+using PharmacyEdition.Service.Services;
+
+namespace PharmacyEdition.Web.Controllers;
+
+public class UsersController : Controller
+{
+    private readonly IUserService userService = new UserService();
+
+
+    public async Task<IActionResult> Index()
+    {
+        var users = await this.userService.GetAllAsync();
+        return View(users.Value);
+    }
+
+    public async Task<IActionResult> Create(UserCreationDto model)
+    {
+        var createdUser = await this.userService.AddAsync(model);
+        return View(createdUser.Value);
+    }
+}
